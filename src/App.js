41a0851ee990673576b48e965ea4cde0fb0cfd1d
PractiseSir <practisesir12@gmail.com>
2024-05-02 18:54:05 +0545

    Added the profile page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Blog from "./pages/Blog/Blog";
 import Login from "./pages/Login-Signup/Login";
 import Signup from "./pages/Login-Signup/Signup";
 import ProfileBlogs from "./pages/BlogDetails/ProfileBlogs";
+import Profile from "./pages/Profile/Profile";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/profileblogs' element={<ProfileBlogs />} />
+          <Route path='/profile' element={<Profile />} />
 
 
         </Routes>
--- src/pages/homePage/Aboutin.js
+++ src/pages/homePage/Aboutin.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Aboutin = () => {
   return (
@@ -44,9 +45,11 @@ const Aboutin = () => {
           </div>
         </div>
         <div className='text-center py-4'>
-          <button className='bg-[#e97442] py-2 px-5 rounded'>
-            BUTTON
-          </button>
+          <Link to='/profile'>
+            <button className='bg-[#e97442] py-2 px-5 rounded'>
+              BUTTON
+            </button>
+          </Link>
         </div>
 
       </div>
