c4256f329ce46fe1fde79fff000bdcb807bce277
notryno <ryanclepcha@gmail.com>
2024-05-10 02:47:29 +0545

    Add Blog

--- src/App.js
+++ src/App.js
@@ -7,22 +7,21 @@ import Login from "./pages/Login-Signup/Login";
 import Signup from "./pages/Login-Signup/Signup";
 import ProfileBlogs from "./pages/BlogDetails/ProfileBlogs";
 import Profile from "./pages/Profile/Profile";
+import AddBlog from "./pages/Blog/AddBlog";
 
 function App() {
   return (
     <div className="overflow-hidden">
-
-      <BrowserRouter >
+      <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/blog' element={<Blog />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/profileblogs' element={<ProfileBlogs />} />
-          <Route path='/profile' element={<Profile />} />
-
-
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profileblogs" element={<ProfileBlogs />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/add-blog" element={<AddBlog />} />
         </Routes>
       </BrowserRouter>
     </div>
